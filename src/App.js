d1923310a4c81c6f688faddbda0560f80e2ddcbf
Graham Morrison <morpole@gmail.com>
2023-08-31 18:14:15 +0100

    added some API practice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import contactList from "./contacts";
+import { useState, useEffect } from 'react';
 
 
 const Person = ({name, lastName, age, address}) => {
@@ -32,9 +33,72 @@ const People = () => {
 }
 
 
+
+
+const BreedList = () => {
+  const [breeds, setBreeds] = useState([]);
+  const [selectedBreed, setSelectedBreed] = useState('');
+  const [breedImages, setBreedImages] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(response => response.json())
+      .then(data => {
+        const breedNames = Object.keys(data.message);
+        setBreeds(breedNames);
+      })
+      .catch(error => console.error('Error fetching breed list:', error));
+  }, []);
+
+  const handleBreedSelect = breed => {
+    setSelectedBreed(breed);
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random/10`)
+      .then(response => response.json())
+      .then(data => {
+        setBreedImages(data.message);
+      })
+      .catch(error => console.error('Error fetching breed images:', error));
+  };
+
+  return (
+    <div>
+      <h1>Dog Breed Browser</h1>
+      <div>
+        <h2>Select a Breed</h2>
+        <select value={selectedBreed} onChange={event => handleBreedSelect(event.target.value)}>
+          <option value="">Select a breed</option>
+          {breeds.map(breed => (
+            <option key={breed} value={breed}>
+              {breed}
+            </option>
+          ))}
+        </select>
+      </div>
+      {selectedBreed && (
+        <div>
+          <h2>{selectedBreed}</h2>
+          <div>
+            {breedImages.map(imageUrl => (
+              <img style={{ width: "200px", height: "200px" }} key={imageUrl} src={imageUrl} alt={`${selectedBreed} dog`} />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+
+
+
+
+
+
 export default function App() {
   return (
     <div className='App'>
+    <BreedList/>
     <People/>
     <Person name='Graham' lastName='Morrison' address='55 Waveney Rd'age={30} />
     </div> 
