b9f8d8e606ed1b09d0d71a893b632415bf0c4f88
Graham Morrison <morpole@gmail.com>
2023-08-31 17:02:46 +0100

    added some css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
+import contactList from "./contacts";
 
 
 const Person = ({name, lastName, age, address}) => {
   return (
     <>
-    <h2>Name: {name}</h2>
+    <h2 className="blue">Name: {name}</h2>
     <h2>Last Name: {lastName}</h2>
     <h2>Address: {age}</h2>
     <h2>Age: {address}</h2>
@@ -13,8 +14,29 @@ const Person = ({name, lastName, age, address}) => {
   )
 }
 
+const People = () => {
+  return(
+    <>
+    {contactList.map(contacts =>
+      <div className='box'>
+      <h2>Name: {contacts.name}</h2>
+      <p>Last Name: {contacts.lastname}</p>
+      <p>Age: {contacts.age}</p>
+      <p>Address: {contacts.address}</p>
+      <p>Telephone: {contacts.telephone}</p>
+      <p>---------------------------------</p>
+      </div>
+      )}
+      </>
+  )
+}
+
+
 export default function App() {
   return (
-    <Person className='App' name='Graham' lastName='Morrison' address='55 Waveney Rd'age={30} />
+    <div className='App'>
+    <People/>
+    <Person name='Graham' lastName='Morrison' address='55 Waveney Rd'age={30} />
+    </div> 
   )
 }
---
+++ src/contacts.js
@@ -0,0 +1,85 @@
+const contactList = [
+    {
+      id: 1,
+      name: "John",
+      lastname: "Doe",
+      age: 30,
+      address: "123 Main St, City",
+      telephone: "123-456-7890"
+    },
+    {
+      id: 2,
+      name: "Jane",
+      lastname: "Smith",
+      age: 25,
+      address: "456 Elm St, Town",
+      telephone: "987-654-3210"
+    },
+    {
+      id: 3,
+      name: "Michael",
+      lastname: "Johnson",
+      age: 40,
+      address: "789 Oak St, Village",
+      telephone: "555-123-4567"
+    },
+    {
+      id: 4,
+      name: "Emily",
+      lastname: "Williams",
+      age: 28,
+      address: "567 Birch St, Suburb",
+      telephone: "222-333-4444"
+    },
+    {
+      id: 5,
+      name: "David",
+      lastname: "Miller",
+      age: 35,
+      address: "890 Maple St, Countryside",
+      telephone: "777-888-9999"
+    },
+    {
+      id: 6,
+      name: "Sophia",
+      lastname: "Brown",
+      age: 22,
+      address: "345 Pine St, Hamlet",
+      telephone: "444-555-6666"
+    },
+    {
+      id: 7,
+      name: "Oliver",
+      lastname: "Jones",
+      age: 29,
+      address: "678 Cedar St, Ranch",
+      telephone: "666-777-8888"
+    },
+    {
+      id: 8,
+      name: "Emma",
+      lastname: "Wilson",
+      age: 31,
+      address: "123 Spruce St, Farm",
+      telephone: "888-999-0000"
+    },
+    {
+      id: 9,
+      name: "Liam",
+      lastname: "Anderson",
+      age: 27,
+      address: "456 Oak St, Village",
+      telephone: "111-222-3333"
+    },
+    {
+      id: 10,
+      name: "Ava",
+      lastname: "Martinez",
+      age: 24,
+      address: "789 Elm St, Town",
+      telephone: "444-555-6666"
+    }
+  ];
+  
+  export default contactList;
+  
