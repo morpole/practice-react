214b2a9ab75c8427617054550c4dccd69172f99c
morpole <morpole@yahoo.com>
2023-09-09 11:13:31 +0100

    tidy

--- src/App.js
+++ src/App.js
@@ -1,29 +1,150 @@
-import React from "react";
-import './App.css';
-// import contactList from "./contacts";
-import { useState, useEffect } from 'react';
+// import React from "react";
+// import './App.css';
+// // import contactList from "./contacts";
+// import { useState, useEffect } from 'react';
 
+// // const Person = ({name, lastName, age, address}) => {
+// //   return (
+// //     <>
+// //     <h2 className="blue">Name: {name}</h2>
+// //     <h2>Last Name: {lastName}</h2>
+// //     <h2>Address: {age}</h2>
+// //     <h2>Age: {address}</h2>
+// //     <h2>*********************************</h2>
+// //     </>
+// //   )
+// // }
 
+// // const People = () => {
+// //   return(
+// //     <>
+// //     {contactList.map(contacts =>
+// //       <div className='box'>
+// //       <h2>Name: {contacts.name}</h2>
+// //       <p>Last Name: {contacts.lastname}</p>
+// //       <p>Age: {contacts.age}</p>
+// //       <p>Address: {contacts.address}</p>
+// //       <p>Telephone: {contacts.telephone}</p>
+// //       <p>---------------------------------</p>
+// //       </div>
+// //       )}
+// //       </>
+// //   )
+// // }
 
+// // const BreedList = () => {
+// //   const [breeds, setBreeds] = useState([]);
+// //   const [selectedBreed, setSelectedBreed] = useState('');
+// //   const [breedImages, setBreedImages] = useState([]);
 
+// //   useEffect(() => {
+// //     fetch('https://dog.ceo/api/breeds/list/all')
+// //       .then(response => response.json())
+// //       .then(data => {
+// //         const breedNames = Object.keys(data.message);
+// //         setBreeds(breedNames);
+// //       })
+// //       .catch(error => console.error('Error fetching breed list:', error));
+// //   }, []);
 
+// //   const handleBreedSelect = breed => {
+// //     setSelectedBreed(breed);
+// //     fetch(`https://dog.ceo/api/breed/${breed}/images/random/10`)
+// //       .then(response => response.json())
+// //       .then(data => {
+// //         setBreedImages(data.message);
+// //       })
+// //       .catch(error => console.error('Error fetching breed images:', error));
+// //   };
 
-//Doglist Breed Selector from DogAPI
+// //   return (
+// //     <div>
+// //       <h1>Dog Breed Browser</h1>
+// //       <div>
+// //         <h2>Select a Breed</h2>
+// //         <select value={selectedBreed} onChange={event => handleBreedSelect(event.target.value)}>
+// //           <option value="">Select a breed</option>
+// //           {breeds.map(breed => (
+// //             <option key={breed} value={breed}>
+// //               {breed}
+// //             </option>
+// //           ))}
+// //         </select>
+// //       </div>
+// //       {selectedBreed && (
+// //         <div>
+// //           <h2>{selectedBreed}</h2>
+// //           <div>
+// //             {breedImages.map(imageUrl => (
+// //               <img style={{ width: "200px", height: "200px" }} key={imageUrl} src={imageUrl} alt={`${selectedBreed} dog`} />
+// //             ))}
+// //           </div>
+// //         </div>
+// //       )}
+// //     </div>
+// //   );
+// // };
 
+// // const UseStateBasics = () => {
+// //   const [greeting, setGreeting] = useState('Rando-ness');
+// //   const handleChange = () => {
+// //    setGreeting(greeting === 'Rando-ness' ? 'I changed it!' : 'randoness');
+// //   }
+// //   return (
+// //     <>
+// //     <h1>{greeting}</h1>
+// //     <button onClick={handleChange}>Click to Change</button>
+// //     </>
+// //   )
+// // }
 
-// const BreedList = () => {
-//   const [breeds, setBreeds] = useState([]);
-//   const [selectedBreed, setSelectedBreed] = useState('');
-//   const [breedImages, setBreedImages] = useState([]);
+// // const UseStateArray = () => {
+// //   const [people, setPeople] = useState(contactList);
+
+// const Person = ({name, lastName, age, address}) => {
+//   return (
+//     <>
+//     <h2 className="blue">Name: {name}</h2>
+//     <h2>Last Name: {lastName}</h2>
+//     <h2>Address: {age}</h2>
+//     <h2>Age: {address}</h2>
+//     <h2>*********************************</h2>
+//     </>
+//   )
+// }
+
+// const People = () => {
+//   return(
+//     <>
+//     {contactList.map(contacts =>
+//       <div className='box'>
+//       <h2>Name: {contacts.name}</h2>
+//       <p>Last Name: {contacts.lastname}</p>
+//       <p>Age: {contacts.age}</p>
+//       <p>Address: {contacts.address}</p>
+//       <p>Telephone: {contacts.telephone}</p>
+//       <p>---------------------------------</p>
+//       </div>
+//       )}
+//       </>
+//   )
+// }
+
+// // second argument
+
+//   const getUsers = async () => {
+//     try{
+//     const response = await fetch(url);
+
+//     const users = await response.json();
+//     setUsers(users);
+//   } catch (error){
+//     console.error('Error fetching data', error);
+//   }
+//   };
 
 //   useEffect(() => {
-//     fetch('https://dog.ceo/api/breeds/list/all')
-//       .then(response => response.json())
-//       .then(data => {
-//         const breedNames = Object.keys(data.message);
-//         setBreeds(breedNames);
-//       })
-//       .catch(error => console.error('Error fetching breed list:', error));
+//     getUsers();
 //   }, []);
 
 //   const handleBreedSelect = breed => {
@@ -64,9 +185,6 @@ import { useState, useEffect } from 'react';
 //   );
 // };
 
-
-//Basic useState example
-
 // const UseStateBasics = () => {
 //   const [greeting, setGreeting] = useState('Rando-ness');
 //   const handleChange = () => {
@@ -80,175 +198,84 @@ import { useState, useEffect } from 'react';
 //   )
 // }
 
-//**Fetch API Example Github Users**
+// const UseStateArray = () => {
+//   const [people, setPeople] = useState(contactList);
 
-// const url = 'https://api.github.com/users';
-
-
-// const UseEffectFetchData = () => {
-//   const [users, setUsers] = useState([])
-
-//   const getUsers = async () => {
-//     const response = await fetch(url);
-//     const users = await response.json()
-//     setUsers(users);
-//   }
-
-//   useEffect(() => {
-//     getUsers();
-//   }, []);
+//   const removeItem = (id) => {
+//     let newPeople = people.filter((person) => person.id !== id);
+//     setPeople(newPeople);
+//   };
 //   return (
 //     <>
-//       <h3>github users display </h3>
-//       <ul className='users'>
-//         {users.map((user) => {
-//           const { id, login, avatar_url, html_url } = user;
-//           return (
-//             <li key={id}>
-//               <img src={avatar_url} alt={login} />
-//               <div>
-//                 <h4>{login}</h4>
-//                 <a href={html_url}>profile</a>
-//               </div>
-//             </li>
-//           );
-//         })}
-//       </ul>
+//       {people.map((person) => {
+//         const { id, name, lastname} = person;
+//         return (
+//           <div key={id} className='item'>
+//             <h4>{name}</h4>
+//             <h4>{lastname}</h4>
+//             <button onClick={() => removeItem(id)}>remove</button>
+//           </div>
+//         );
+//       })}
+//       <button className='btn' onClick={() => setPeople([])}>
+//         clear items
+//       </button>
 //     </>
 //   );
 // };
 
+const url = "https://api.github.com/users";
 
+// second argument
 
-  
- 
+const UseEffectFetchData = () => {
+  const [users, setUsers] = useState([]);
 
+  const getUsers = async () => {
+    try {
+      const response = await fetch(url);
 
-  //   const url = 'https://dog.ceo/api/breeds/image/random/5';
+      const users = await response.json();
+      setUsers(users);
+    } catch (error) {
+      console.error("Error fetching data", error);
+    }
+  };
 
-  //   const DoggiePics = () => {
-  //     const [dogs, setDogs] = useState([]);
-
-  //     useEffect(()=>{
-  //       fetch(url)
-  //       .then(res => {
-  //         return res.json();
-  //       })
-  //       .then(data => {
-  //         if(data.status === 'success'){
-  //           setDogs(data.message)
-  //         } else {
-  //           console.error('Failed to fetch dog images');
-  //         }
-  //       })
-  //       .catch(error => {
-  //         console.error("Error fetching dog images:", error);
-  //       });
-  //     },[])
-
-  //    return (
-  //         <>
-  //           <h3>dog pics displayer </h3>
-  //           <ul >
-  //             {dogs.map((message, index) => (
-  //                 <li  key={index} className="users img">
-  //                   <img className='users img' src={message} alt={'Dog'} />
-  //                 </li>
-
-  //             ))}
-  //           </ul>
-  //         </>
-  //       );
-  
-  // }
-
-
-
-
-  //   return (
-  //         <>
-  //           <h3>dog pics displayer </h3>
-  //           <ul >
-  //             {dogs.map((message, index) => (
-  //                 <li  key={index} className="users img">
-  //                   <img className='users img' src={message} alt={'Dog'} />
-  //                 </li>
-
-  //             ))}
-  //           </ul>
-  //         </>
-  //       );
-  
-  // }
-  
-
- 
-
-  // const WorldTime = () => {
-  //   const [data, setData] = useState(null);
-  
-    // useEffect(() => {
-    //   const fetchData = async () => {
-    //     const url = 'http://worldtimeapi.org/api/timezone/Europe/Dublin';
-    //     try {
-    //       const response = await fetch(url);
-    //       if (!response.ok) {
-    //         throw new Error(`HTTP Error! Status: ${response.status}`);
-    //       }
-    //       const responseData = await response.json();
-    //       setData(responseData);
-    //     } catch (error) {
-    //       console.error('An error occurred:', error);
-    //     }
-    //   };
-  
-    //   fetchData();
-    // }, []);
-  
-   
-
-  //   return (
-  //     <div className="App">
-  //       <h1>Data from WorldTimeAPI</h1>
-  //       <pre>{JSON.stringify(data, null, 1)}</pre>
-        
-  //     </div>
-  //   );
-  // };
-  
-
-
-  
-
-
-const BigFetcher = () => {
-
-  const [data, setData] = useState(null); 
-
-  useEffect(()=>{
-    const getData = async () => {
-    const url = 'http://worldtimeapi.org/api/timezone/Europe/Dublin';
-    const response =  await fetch(url);
-    const responseData = await response.json();
-    setData(responseData);
-    };
-    getData();
-  },[])
-
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
-    <div>
-      <h2>Big Time Travelor Sham!</h2>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  )
+    <>
+      <h3>github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
-
 
 export default function App() {
   return (
-    <div className='App'>
-    {/* <WorldTime/> */}
-   <BigFetcher/>
-    </div> 
-  )
+    <div className="App">
+      <UseEffectFetchData />
+      {/* <BreedList/> */}
+      {/* <People/>
+    <Person name='Graham' lastName='Morrison' address='55 Waveney Rd'age={30} />
+    <UseStateBasics/>
+    <h2>**********************************</h2>
+    <UseStateArray/> */}
+    </div>
+  );
 }
